85f11d0f8a0f310a7bae2f80bab0192b9774ce3a
MFel33 <104394609+MatheusFelipe1988@users.noreply.github.com>
2023-09-03 16:30:17 -0400

    Delete demo directory
    Paste errada

--- demo/demo/src/main/java/com/exemplo/demo/repository/Produtorepository.java
+++
@@ -1,9 +0,0 @@
-package com.exemplo.demo.repository;
-
-import com.exemplo.demo.domain.Produto;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface Produtorepository extends CrudRepository<Produto, String> {
-}
