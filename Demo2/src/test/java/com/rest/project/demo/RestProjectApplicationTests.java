72cb36088c230843ca7f56863da6b3061fbbf34d
MFel33 <104394609+MatheusFelipe1988@users.noreply.github.com>
2023-09-03 16:28:19 -0400

    Add files via upload

---
+++ Demo2/src/test/java/com/rest/project/demo/RestProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rest.project.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
